65750ee7a3fa2b047056ebbf4965e607abc98ce0
linux4geek <97961580+linux4geek@users.noreply.github.com>
2022-01-21 18:08:24 +0800

    更新c++11测试

--- build/cpp11.cpp
+++ build/cpp11.cpp
@@ -1,10 +1,12 @@
 #include "cpp11.h"
 #include <iostream>
 #include <string>
+#include <complex>
 
 using namespace std;
 
 void Cpp11Main(int argc, char** argv) {
-    int 
+    int nData = { 1 };
+    complex<int> comData = { 2, 3 };
 
 }
